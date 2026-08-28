b1bf2ba09b71f8fd98fb887e9cffff976e0fb63e
Amchege <ibrahkaris@gmail.com>
2026-08-29 01:11:25 +0300

    fix: raw GA4 + GTM scripts for Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { JsonLd } from "@/components/seo/json-ld";
@@ -63,42 +62,52 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <head>
   {/* Meta Pixel */}
-  <Script id="meta-pixel" strategy="afterInteractive">
-    {`
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '672113158769223');
-      fbq('track', 'PageView');
-    `}
-  </Script>
+  {/* eslint-disable-next-line @next/next/no-script-in-head */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '672113158769223');
+        fbq('track', 'PageView');
+      `,
+    }}
+  />
 
   {/* GA4 */}
-  <Script src={`https://www.googletagmanager.com/gtag/js?id=G-CK73F192MY`} strategy="afterInteractive" />
-  <Script id="ga4-config" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-CK73F192MY');
-    `}
-  </Script>
+  {/* eslint-disable-next-line @next/next/no-script-in-head */}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-CK73F192MY" />
+  {/* eslint-disable-next-line @next/next/no-script-in-head */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-CK73F192MY');
+      `,
+    }}
+  />
 
   {/* GTM */}
-  <Script id="gtm" strategy="afterInteractive">
-    {`
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-NC2RZQRN');
-    `}
-  </Script>
+  {/* eslint-disable-next-line @next/next/no-script-in-head */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NC2RZQRN');
+      `,
+    }}
+  />
 </head>
       <body className="min-h-screen flex flex-col antialiased">
         <JsonLd data={generateOrganizationSchema()} />
